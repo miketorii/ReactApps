5741449fede3278f1c1cfd2377aeb05e284bd026
miketorii <miketorii@gmail.com>
2024-08-31 11:27:03 +0900

    update test

--- appts/src/SimpleButton.test.tsx
+++ appts/src/SimpleButton.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { SimpleButton } from "./SimpleButton";
-import userEvent from "@testing-library/user-event";
+//import userEvent from "@testing-library/user-event";
+
 test ( "Change on off when the button is clicked", async () => {
-    const user = userEvent.setup();
+//    const user = userEvent.setup();
 
     render(<SimpleButton />);
 
     const simpleButton = screen.getByRole("button");
+
 //    expect(simpleButton).toContainEqual(<button>OFF</button>);
 
-    await user.click(simpleButton);
+//    await user.click(simpleButton);
     
 });
