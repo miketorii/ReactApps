6e3298cace62e6234352f9e30313ba8df4930fef
miketorii <miketorii@gmail.com>
2024-09-07 01:29:09 +0000

    add component

---
+++ apptsvm/src/components/Hello.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Hello() {
+  const onMyClickFunc = () => { alert('You clicked!'); };
+  
+  const text = 'If you click here, alert is displayed from this component';
+
+  return (
+    <div onClick={onMyClickFunc} >
+        {text}
+    </div>
+  );
+}
+
+export default Hello;
+
+
+
+
