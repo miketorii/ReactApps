80f31faf7187b255679b5a91931e48bdd9ada0f2
miketorii <miketorii@gmail.com>
2024-09-12 12:32:51 +0000

    update nextjs

--- appnextvm/app/page.tsx
+++ appnextvm/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import { GetStaticProps, NextPage, NextPageContext } from 'next';
+import Head from 'next/head';
+
+type SSGProps = {
+     message: string
+};
+
+const SSGPage: NextPage<SSGProps> = (props) => {
+      const { message } = props;
+
        return (
        	      <div>
-	       This is NEXT.js page!
+	      	      <Head>
+			<title>Static Site Generation</title>
+	      	      </Head>
+		      <main>
+			<p>This is NEXT.js SSG page!</p>
+			<p>{message}</p>
+		      </main>
 	      </div>
        );
 }
+
+export default SSGPage;
