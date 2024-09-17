3d5c60097d8ee08772bd1a91f364442e78ac99c4
miketorii <miketorii@gmail.com>
2024-09-17 12:54:44 +0000

    add styled span button

--- appnextvm/app/devicelist/page.tsx
+++ appnextvm/app/devicelist/page.tsx
@@ -5,7 +5,32 @@ import Link from 'next/link';
 import styled from 'styled-components';
 
 const H1 = styled.h1`
-      color: red;
+      color: gray;
+`
+
+const Badge = styled.span`
+      padding: 8px, 16px;
+      font-weight: bold;
+      text-align:  center;
+      color:	   white;
+      background:  gray;
+      border-radius:	2px;
+`
+type ButtonProps = {
+     color: string,
+     backgroundColor: string
+};
+
+const Button = styled.button<ButtonProps>`
+      color: ${(props) => props.color};
+      background: ${(props) => props.backgroundColor};
+      border: 2px solid ${(props) => props.color};
+
+      font-size: 2em;
+      margin:	 1em;
+      padding: 0.25em 1em;
+      border-radius: 8px;
+      cursor: pointer;
 `
 
 export default function DeviceListPage(){
@@ -30,8 +55,14 @@ export default function DeviceListPage(){
 
 	return (
               <div>
+
 		<H1>This is devicelist page.</H1>
 		<h2>{devicedata}</h2>
+		<p />
+		<Badge>Device List</Badge>
+		<p />
+		<Button backgroundColor="#1E90FF" color="white">Detail</Button>
+		<Button backgroundColor="transparent" color="#FF0000">More</Button>
 		<p />
 		<button onClick={getData}>get</button>
 		<Link href="/">Back</Link>
